983ad7128adb18a05ab69e3188e53569645d34c5
Ricardo García Vega <bigardone@gmail.com>
2016-02-29 15:32:15 +0100

    Database seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,10 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Kairos.Repo.insert!(%Kairos.SomeModel{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Kairos.{Repo, User}
+
+Repo.delete_all User
+
+[
+  %{first_name: "Bruno", email: "bruno@diacode.com", password: "12345678"},
+  %{first_name: "Ricardo", email: "ricardo@diacode.com", password: "12345678"}
+]
+|> Enum.map(&User.changeset(%User{}, &1))
+|> Enum.each(&Repo.insert!(&1))
