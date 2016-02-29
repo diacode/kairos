87957d8f91f034627a9c6ab2759fe666ac7386fa
Ricardo García Vega <bigardone@gmail.com>
2016-02-29 17:09:53 +0100

    Handle user change message in user channel

--- web/channels/user_channel.ex
+++ web/channels/user_channel.ex
@@ -1,6 +1,8 @@
 defmodule Kairos.UserChannel do
   use Kairos.Web, :channel
 
+  alias Kairos.{Repo, User}
+
   def join("users:" <> user_id, _params, socket) do
     current_user = socket.assigns.current_user
 
@@ -8,6 +10,20 @@ defmodule Kairos.UserChannel do
       {:ok, socket}
     else
       {:error, %{reason: "Invalid user"}}
+    end
+  end
+
+  def handle_in("user:update", params, socket) do
+    current_user = socket.assigns.current_user
+
+    current_user
+    |> User.update_changeset(params)
+    |> Repo.update
+    |> case do
+      {:ok, user} ->
+        {:reply, {:ok, %{user: user}}, socket}
+      {:error, _changeset} ->
+        {:reply, {:error,%{reason: "Invalid"}}, socket}
     end
   end
 end
