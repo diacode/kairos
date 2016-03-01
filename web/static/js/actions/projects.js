535c8a224197ac328590a87f17d78cf1b26ec90e
Ricardo García Vega <bigardone@gmail.com>
2016-03-01 10:44:32 +0100

    Fetch projects action

--- web/static/js/actions/projects.js
+++ web/static/js/actions/projects.js
@@ -1,5 +1,19 @@
 import Constants from '../constants';
 
+export function fetchProjects(channel) {
+  return dispatch => {
+    dispatch({ type: Constants.PROJECTS_FETCH_START });
+
+    channel.push('user:projects')
+    .receive('ok', (payload) => {
+      dispatch(setProjects(payload.projects));
+    })
+    .receive('error', (payload) => {
+      dispatch(setProjects([]));
+    });
+  };
+}
+
 export function setProjects(projects) {
   return dispatch => {
     dispatch({
--- web/static/js/reducers/projects.js
+++ web/static/js/reducers/projects.js
@@ -1,13 +1,17 @@
 import Constants from '../constants';
 
 const initialState = {
-  items: null,
+  fetching: false,
+  projects: null,
 };
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
+    case Constants.PROJECTS_FETCH_START:
+      return { ...state, fetching: true };
+
     case Constants.PROJECTS_SET:
-      return { ...initialState, items: action.projects };
+      return { ...initialState, projects: action.projects, fetching: false };
 
     default:
       return state;
