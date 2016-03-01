ddcc1b08b2571828b90445a85047ccabadb689d8
Ricardo García Vega <bigardone@gmail.com>
2016-03-01 10:43:59 +0100

    Enhanced route and main container

--- web/static/js/routes/index.js
+++ web/static/js/routes/index.js
@@ -1,20 +1,48 @@
-import { IndexRoute, Route }        from 'react-router';
-import React                        from 'react';
-import MainLayout                   from '../layouts/main';
-import AuthenticatedContainer       from '../containers/authenticated';
-import HomeIndexView                from '../views/home';
-import RegistrationsNew             from '../views/registrations/new';
-import SessionsNew                  from '../views/sessions/new';
-import SettingsIndexView            from '../views/settings/index';
+import { IndexRoute, Route }    from 'react-router';
+import React                    from 'react';
+import { routeActions }         from 'react-router-redux';
+import MainLayout               from '../layouts/main';
+import AuthenticatedContainer   from '../containers/authenticated';
+import HomeIndexView            from '../views/home';
+import RegistrationsNew         from '../views/registrations/new';
+import SessionsNew              from '../views/sessions/new';
+import SettingsIndexView        from '../views/settings/index';
+import { fetchProjects }        from '../actions/projects';
+import Actions                  from '../actions/sessions';
 
-export default (
-  <Route component={MainLayout}>
-    <Route path="/sign_up" component={RegistrationsNew} />
-    <Route path="/sign_in" component={SessionsNew} />
+export default function configRoutes(store) {
+  const _ensureAuthenticated = (nextState, replace, callback) => {
+    const { dispatch } = store;
+    const { session } = store.getState();
+    const { currentUser } = session;
 
-    <Route path="/" component={AuthenticatedContainer}>
-      <IndexRoute component={HomeIndexView} />
-      <Route path="settings" component={SettingsIndexView} />
+    if (!currentUser && localStorage.getItem('phoenixAuthToken')) {
+      dispatch(Actions.currentUser());
+    } else if (!localStorage.getItem('phoenixAuthToken')) {
+      replace('/sign_in');
+    }
+
+    callback();
+  };
+
+  const _fetchProjects = (nextState, replace) => {
+    const { session } = store.getState();
+    const { channel } = session;
+
+    if (channel === null) return false;
+
+    store.dispatch(fetchProjects(channel));
+  };
+
+  return (
+    <Route component={MainLayout}>
+      <Route path="/sign_up" component={RegistrationsNew} />
+      <Route path="/sign_in" component={SessionsNew} />
+
+      <Route path="/" component={AuthenticatedContainer} onEnter={_ensureAuthenticated}>
+        <IndexRoute component={HomeIndexView} onEnter={_fetchProjects} />
+        <Route path="settings" component={SettingsIndexView} />
+      </Route>
     </Route>
-  </Route>
-);
+  );
+}
