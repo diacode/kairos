48ca21cb9dba4f5b1c673d13fff349bb42d2d2f7
Bruno <brunobayon@gmail.com>
2016-03-01 12:32:44 +0100

    render onboarding form if settings are not set

--- web/static/js/views/home/index.js
+++ web/static/js/views/home/index.js
@@ -1,14 +1,12 @@
-import React                from 'react';
-import { connect }          from 'react-redux';
-import classnames           from 'classnames';
-
-import { setDocumentTitle } from '../../utils';
-import { setProjects, fetchProjects }      from '../../actions/projects';
+import React                            from 'react';
+import { connect }                      from 'react-redux';
+import classnames                       from 'classnames';
+import { setDocumentTitle }             from '../../utils';
+import { setProjects, fetchProjects }   from '../../actions/projects';
+import OnboardingForm                   from '../../components/onboarding';
 
 class HomeIndexView extends React.Component {
   componentWillReceiveProps(nextProps) {
-    const { fetching } = nextProps;
-
     this._fetchProjects(nextProps);
   }
 
@@ -19,15 +17,25 @@ class HomeIndexView extends React.Component {
   }
 
   _fetchProjects(props) {
-    const { channel, dispatch, projects, fetching } = props;
+    const { channel, dispatch, projects, fetching, currentUser } = props;
 
+    if (currentUser === null || currentUser.settings === null || currentUser.settings.pivotal_tracker_api_token === null || currentUser.settings.pivotal_tracker_api_token === '') return false;
     if (channel === null || projects != null || fetching) return false;
 
     dispatch(fetchProjects(channel));
   }
 
   _renderProjects() {
-    const { projects, fetching } = this.props;
+    const { projects, fetching, currentUser, dispatch, channel } = this.props;
+
+    if (currentUser.settings === null || currentUser.settings.pivotal_tracker_api_token === null || currentUser.settings.pivotal_tracker_api_token === '') {
+      return (
+        <OnboardingForm
+          currentUser={currentUser}
+          dispatch={dispatch}
+          channel={channel} />
+      );
+    }
 
     if (fetching) return this._renderFetching();
     if (projects === null) return false;
@@ -56,8 +64,12 @@ class HomeIndexView extends React.Component {
   }
 
   render() {
+    const { currentUser } = this.props;
+
+    if (!currentUser) return false;
+
     return (
-      <div className='view-container home index'>
+      <div className="view-container home index">
         {::this._renderProjects()}
       </div>
     );
