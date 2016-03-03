68afc2139a7d7e98306fb5b2eaa318eb30378eaf
Ricardo García Vega <bigardone@gmail.com>
2016-03-03 11:06:35 +0100

    Updated extractor version and fixed error

--- web/channels/project_channel.ex
+++ web/channels/project_channel.ex
@@ -4,11 +4,11 @@ defmodule Kairos.ProjectChannel do
 
   def join("project:" <> project_id, _params, socket) do
     Logger.info "Joined to ProjectChannel"
-    
+
     current_user = socket.assigns.current_user
 
     client = ExTracker.Client.new %{access_token: current_user.settings.pivotal_tracker_api_token}
-    project = ExTracker.Projects.find(project_id, client)
+    project = ExTracker.Projects.find(client, project_id)
     stories = ExTracker.Stories.list(client, project_id)
 
     {:ok, %{project: project, stories: stories}, socket}
