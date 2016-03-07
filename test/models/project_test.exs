6007b0ddcdadc5e540c82cebbf18cdeca0c837b4
Ricardo García Vega <bigardone@gmail.com>
2016-03-07 18:08:32 +0100

    Project migration, model and test

---
+++ test/models/project_test.exs
@@ -0,0 +1,18 @@
+defmodule Kairos.ProjectTest do
+  use Kairos.ModelCase
+
+  alias Kairos.Project
+
+  @valid_attrs %{description: "some content", name: "some content", pivotal_tracker_id: 42, toggle_id: 42}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = Project.changeset(%Project{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = Project.changeset(%Project{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
