7e2be68fdf2fd0736a44ca1d990870d6696de5c1
Ricardo García Vega <bigardone@gmail.com>
2016-03-08 10:46:54 +0100

    Project form reducer

---
+++ web/static/js/reducers/project_form.js
@@ -0,0 +1,13 @@
+import Constants from '../constants';
+
+const initialState = {
+  pivotalTrackerProjects: [],
+  togglProjects: [],
+};
+
+export default function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
