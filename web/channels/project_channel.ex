9fb9ad735aa174b63852a03abe9708ca0f99679f
Ricardo García Vega <bigardone@gmail.com>
2016-03-08 17:49:58 +0100

    Get data from server

--- web/channels/project_channel.ex
+++ web/channels/project_channel.ex
@@ -1,15 +1,13 @@
 defmodule Kairos.ProjectChannel do
   use Kairos.Web, :channel
   require Logger
-  alias Kairos.{Repo, Project}
 
   def join("project:" <> project_id, _params, socket) do
     Logger.info "Joined to ProjectChannel"
 
-    project = Project
-      |> Repo.get!(project_id)
-
-    stories = Kairos.Story.Fetcher.get_stories(project.pivotal_tracker_id)
+    %{project: project, stories: stories} = project_id
+      |> String.to_integer
+      |> Kairos.Project.Server.state
 
     {:ok, %{project: project, stories: stories}, assign(socket, :project, project)}
   end
