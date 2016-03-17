c86e1cc1bcd6fefc747253e73786cfd773a5e685
Ricardo García Vega <bigardone@gmail.com>
2016-03-17 12:40:53 +0100

    Refresh project servers every hour

--- lib/kairos/project/server.ex
+++ lib/kairos/project/server.ex
@@ -5,6 +5,8 @@ defmodule Kairos.Project.Server do
 
   defstruct [
     id: nil,
+    pivotal_tracker_id: nil,
+    toggl_id: nil,
     name: nil,
     description: nil,
     start_date: nil,
@@ -16,6 +18,8 @@ defmodule Kairos.Project.Server do
     total_dedicated_hours: 0,
     velocity: 0
   ]
+
+  def ref(project_id), do: {:global, {:project_id, project_id}}
 
   @doc """
   Creates a new Project process if it doesn't exist. If it's been previously
@@ -35,6 +39,27 @@ defmodule Kairos.Project.Server do
   def init(project) do
     Logger.debug "Starting server for project #{project.id}"
 
+    {:ok, build_data(project)}
+  end
+
+  @doc """
+  Returns the state of the process
+  """
+  def get_data(project_id) do
+    try_call(project_id, :get_data)
+  end
+
+  def handle_call(:get_data, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_info(:refresh, state) do
+    Logger.debug "Refreshing server data for project #{state.id}"
+
+    {:noreply, build_data(state)}
+  end
+
+  defp build_data(project) do
     stories = get_stories(project.pivotal_tracker_id)
     time_entries = get_time_entries(project.toggl_id, project.start_date)
     total_story_points = total_story_points(stories)
@@ -42,8 +67,10 @@ defmodule Kairos.Project.Server do
     total_estimated_hours = total_estimated_hours(stories)
     total_dedicated_hours = total_dedicated_hours(time_entries)
 
-    state = %__MODULE__{
+    %__MODULE__{
       id: project.id,
+      pivotal_tracker_id: project.pivotal_tracker_id,
+      toggl_id: project.toggl_id,
       name: project.name,
       description: project.description,
       start_date: project.start_date,
@@ -55,15 +82,6 @@ defmodule Kairos.Project.Server do
       total_dedicated_hours: total_dedicated_hours,
       velocity: total_velocity(total_story_points, total_completed_points, total_estimated_hours, total_dedicated_hours)
     }
-
-    {:ok, state}
-  end
-
-  @doc """
-  Returns the state of the process
-  """
-  def get_data(project_id) do
-    try_call(project_id, :get_data)
   end
 
   defp try_call(project_id, call_function) do
@@ -72,12 +90,6 @@ defmodule Kairos.Project.Server do
       project -> GenServer.call(project, call_function)
     end
   end
-
-  def handle_call(:get_data, _from, state) do
-    {:reply, state, state}
-  end
-
-  defp ref(project_id), do: {:global, {:project_id, project_id}}
 
   defp get_stories(pivotal_tracker_id), do: Kairos.Story.Fetcher.get_stories(pivotal_tracker_id)
   defp get_time_entries(toggl_id, start_date), do: Kairos.TimeEntry.Fetcher.get_time_entries(toggl_id, start_date)
--- lib/kairos/project/starter.ex
+++ lib/kairos/project/starter.ex
@@ -4,19 +4,12 @@ defmodule Kairos.Project.Starter do
   application starts.
   """
   use GenServer
+  require Logger
 
   alias Kairos.{Repo, Project}
 
   def start_link() do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
-  end
-
-  def start_project(project) do
-    GenServer.cast(__MODULE__, {:start_project, project})
-  end
-
-  def get_projects do
-    GenServer.call(__MODULE__, :get_projects)
   end
 
   def init(_) do
@@ -25,7 +18,28 @@ defmodule Kairos.Project.Starter do
       |> Enum.map(&do_start_project/1)
       |> Enum.map(&Task.await/1)
 
+    plan_refresh_projects
+
     {:ok, %{projects: projects}}
+  end
+
+  @doc """
+  Starts a new Kairos.Project.Server
+  """
+  def start_project(project) do
+    GenServer.cast(__MODULE__, {:start_project, project})
+  end
+
+  @doc """
+  Returns all started projects
+  """
+  def get_projects do
+    GenServer.call(__MODULE__, :get_projects)
+  end
+
+
+  def handle_call(:get_projects, _from, %{projects: projects} = state) do
+    {:reply, projects, state}
   end
 
   def handle_cast({:start_project, project}, %{projects: projects} = state) do
@@ -34,8 +48,15 @@ defmodule Kairos.Project.Starter do
     {:noreply, %{state | projects: [project.id | projects]}}
   end
 
-  def handle_call(:get_projects, _from, %{projects: projects} = state) do
-    {:reply, projects, state}
+  def handle_info(:refresh_projects, %{projects: projects} = state) do
+    Logger.debug "Start refreshing projects"
+
+    projects
+    |> Enum.each(&do_refresh_project(&1))
+
+    plan_refresh_projects
+
+    {:noreply, state}
   end
 
   defp do_start_project(project) do
@@ -45,5 +66,18 @@ defmodule Kairos.Project.Starter do
         project.id
       end
     )
+  end
+
+  defp plan_refresh_projects do
+    Process.send_after(self(), :refresh_projects, 60 * 60 * 1000)
+  end
+
+  def do_refresh_project(project_id) do
+    spawn fn ->
+      project_id
+      |> Kairos.Project.Server.ref
+      |> GenServer.whereis
+      |> send(:refresh)
+    end
   end
 end
