2cb5369f5f3f689b448283e13713975c9aac328d
Victor <hopsor@gmail.com>
2016-03-21 16:21:40 +0100

    added server: true to Kairos.Endpoint prod config

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,8 @@ use Mix.Config
 config :kairos, Kairos.Endpoint,
   http: [port: 8889],
   url: [host: "kairos.diacode.com"],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
