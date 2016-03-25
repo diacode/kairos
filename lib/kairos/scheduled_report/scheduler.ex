524014975029559f03b334682d88a1abac7c7065
Victor <hopsor@gmail.com>
2016-03-25 18:35:38 +0100

    Added pdf retrieving from Toggl to `process_notification/1` on `Kairos.ScheduledReport.Scheduler`

--- lib/kairos/scheduled_report/scheduler.ex
+++ lib/kairos/scheduled_report/scheduler.ex
@@ -1,7 +1,11 @@
 defmodule Kairos.ScheduledReport.Scheduler do
   alias Kairos.{Repo, ScheduledReport}
+  use Timex
+  require Logger
 
   def schedule(sr) do
+    Logger.debug "Scheduling report #{sr.id} with frequency #{sr.frequency}"
+
     job = %Quantum.Job{schedule: sr.frequency, task: fn -> process_notification(sr) end}
     Quantum.add_job("scheduled_report_#{sr.id}", job)
   end
@@ -18,6 +22,26 @@ defmodule Kairos.ScheduledReport.Scheduler do
   end
 
   def process_notification(sr) do
-    IO.puts "Notifying ScheduledReport with id #{sr.id}"
+    Logger.debug "Notifying ScheduledReport with id #{sr.id}"
+
+    toggl_api_token = Application.get_env(:kairos, :toggl_api_token)
+    toggl_workspace_id = Application.get_env(:kairos, :toggl_workspace_id)
+    since = Date.today
+    |> Timex.shift(days: -sr.days)
+    |> Timex.format!("%Y-%m-%d", :strftime)
+
+    # Retrieving the report from toggl_api
+    toggl_query = %{
+      workspace_id: toggl_workspace_id,
+      project_ids: sr.project.toggl_id,
+      since: since,
+      until: Date.today |> Timex.format!("%Y-%m-%d", :strftime)
+    }
+
+    pdf = Togglex.Client.new(%{access_token: toggl_api_token}, :reports)
+    |> Togglex.Reports.summary(toggl_query, :pdf)
+
+    # TODO: Replace below code with real email sending
+    File.write!("report.pdf", pdf)
   end
 end
